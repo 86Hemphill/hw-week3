7639655e35aa3a7a00202211b64682856f026685
Logan Hemphill <logan.hemphill86@gmail.com>
2020-03-21 23:15:44 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,65 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lcCriteriaChar = "abcdefghijklmnopqrstuvwxyz";
+var ucCriteriaChar = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+var numCharChar = "0123456789";
+var specCharChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+
+  passwordText.value = password;
+ 
+
+}
+
+function generatePassword() {
+    pwLength = Number(prompt("How long of a password do you need? Enter a number between 8 and 128."));
+    while (pwLength < 8 || pwLength > 128){
+        var pwLength = prompt("Invalid number. How many characters would you like in your password? Must enter between 8 and 128.")
+      }
+    
+    lcCriteria = confirm("Do you want to include lowercase letters?");
+    if (lcCriteria==true) {
+        lcCriteria = lcCriteriaChar;
+    } else {
+        lcCriteria = "";
+    }
+    ucCriteria = confirm("Do you want to include uppercase letters?");
+    if (ucCriteria==true) {
+        ucCriteria = ucCriteriaChar;
+    } else {
+        ucCriteria = "";
+    }
+    numChar = confirm("Do you want to include numbers?");
+    if (numChar==true) {
+        numChar = numCharChar;
+    } else {
+        numChar = "";
+    }
+    specChar = confirm("Do you want to include special characters?");
+    if (specChar==true) {
+        specChar = specCharChar;
+    } else {
+        specChar = "";
+    }
+    
+
+    for (let i = 0; i < pwLength; i++) {
+        const passwordText = "lcCriteria" + "ucCriteria" + "specChar" + "numChar"[Math.floor(Math.random())];
+        
+    }
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
