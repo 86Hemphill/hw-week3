2bbf9d3c71cc2c1415cac919b72c3130d26ae2b9
Logan Hemphill <logan.hemphill86@gmail.com>
2020-03-21 23:58:31 -0400

    homework vol1.1

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var lcCriteriaChar = "abcdefghijklmnopqrstuvwxyz";
-var ucCriteriaChar = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+var ucCriteriaChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numCharChar = "0123456789";
-var specCharChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var specCharChar = "!@#$%^&*()";
+
 
 
 
@@ -22,44 +23,47 @@ function writePassword() {
 
 function generatePassword() {
     pwLength = Number(prompt("How long of a password do you need? Enter a number between 8 and 128."));
+    
     while (pwLength < 8 || pwLength > 128){
         var pwLength = prompt("Invalid number. How many characters would you like in your password? Must enter between 8 and 128.")
       }
     
     lcCriteria = confirm("Do you want to include lowercase letters?");
-    if (lcCriteria==true) {
+    if (lcCriteria == true) {
         lcCriteria = lcCriteriaChar;
     } else {
         lcCriteria = "";
     }
     ucCriteria = confirm("Do you want to include uppercase letters?");
-    if (ucCriteria==true) {
+    if (ucCriteria == true) {
         ucCriteria = ucCriteriaChar;
     } else {
         ucCriteria = "";
     }
     numChar = confirm("Do you want to include numbers?");
-    if (numChar==true) {
+    if (numChar == true) {
         numChar = numCharChar;
     } else {
         numChar = "";
     }
     specChar = confirm("Do you want to include special characters?");
-    if (specChar==true) {
+    if (specChar == true) {
         specChar = specCharChar;
     } else {
         specChar = "";
     }
     
+    var allChar = [lcCriteria, ucCriteria, numChar, specChar];
 
     for (let i = 0; i < pwLength; i++) {
-        const passwordText = "lcCriteria" + "ucCriteria" + "specChar" + "numChar"[Math.floor(Math.random())];
+        passwordText = allChar[Math.floor(Math.random() * pwLength)];
         
     }
+    
+    
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
